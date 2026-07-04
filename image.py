3610a27db9236f599bff829d6f94f46bcebb15b6
inesvar <90340779+inesvar@users.noreply.github.com>
2026-07-04 19:40:02 +0200

    image: Validate input data

--- image.py
+++ image.py
@@ -1,5 +1,16 @@
 class Image:
-    def __init__(self, width, height, scanlines):
-        self.width = width
-        self.height = height
+    def __init__(self, scanlines):
+        if len(scanlines) == 0:
+            raise ValueError("Expected at least one scanline")
+        elif len(scanlines[0]) == 0:
+            raise ValueError("Scanlines must not be empty")
+        elif len(scanlines[0]) % 3 != 0:
+            raise ValueError("Each scanline must contain a multiple of 3 bytes (RGB pixels)")
+        
+        for scanline in scanlines:
+            if len(scanline) != len(scanlines[0]):
+                raise ValueError("Expected all scanlines to have equal length")
+
+        self.height = len(scanlines)
+        self.width = len(scanlines[0]) // 3
         self.scanlines = scanlines
