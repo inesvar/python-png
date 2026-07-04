945d1debe66723dc2c7159650e42d234f95505e0
inesvar <90340779+inesvar@users.noreply.github.com>
2026-07-04 19:00:20 +0200

    main: Rendering a 8192x8192 png

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from image import Image
 from png import Png
 
-image = Image(1, 1, [b"\xFF\x00\x00"])
+image = Image(8192, 8192, [b"\xFF\x00\x00\x00\xFF\x00" * 4096, b"\x00\xFF\x00\xFF\x00\x00" * 4096] * 4096)
 
-Png.write(image, "red_pixel.png", use_mock=True)
+Png.write(image, "checkerboard.png", use_mock=False)
